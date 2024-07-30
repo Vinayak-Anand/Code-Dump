a56c0467562e1932cff71d767fe8607c68267893
Vinayak Anand <vinayakanand010503@gmail.com>
2024-07-30 17:35:06 +0530

    implement mobile navigation

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+
+export const siteConfig = {
+    name: "CodeDump",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "Vinayak Anand",
+    links: {
+      twitter: "https://x.com/anand_vina13963",
+      github: "https://github.com/Vinayak-Anand",
+      // personalSite: "https://jamesshopland.com",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
